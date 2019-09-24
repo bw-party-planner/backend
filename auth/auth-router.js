45ebc5cfa2b4edc54a1e8df6d820013f52706609
Marques Johnson <marques.johnson1992@gmail.com>
2019-09-24 17:31:10 -0400

    try again

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -13,7 +13,7 @@ const express = require('express');
  
   const user = req.body;
    const hash = bcrypt.hashSync(user.password, 10);
-   user.password = hash;
+   const user.password = hash;
  
    Users.add(user)
      .then(saved => {

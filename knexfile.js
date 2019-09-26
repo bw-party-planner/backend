13efde1a3612ab9ad40255bc1b461f1c391b5426
Suparada Saitalae <ann.suparada@gmail.com>
2019-09-26 10:49:59 -0500

    testing done, MVP

--- knexfile.js
+++ knexfile.js
@@ -17,6 +17,19 @@ module.exports = {
       directory: './database/seeds',
     },
   },
+  testing: {
+    client: 'sqlite3',
+    useNullAsDefault: true,
+    connection: {
+      filename: './database/test.db3',
+    },
+    migrations: {
+      directory: './database/migrations',
+    },
+    seeds: {
+      directory: './database/seeds',
+    },
+  },
   production: {
     client: 'pg',
     useNullAsDefault: true,

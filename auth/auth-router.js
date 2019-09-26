48f1c36dd7ab703016980b79e3755ac2d2553d61
Suparada Saitalae <ann.suparada@gmail.com>
2019-09-26 12:30:41 -0500

    register error message

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -17,7 +17,7 @@
        res.status(201).json(saved);
      })
      .catch(error => {
-       res.status(500).json(error);
+       res.status(500).json({ message: 'This username is already taken' });
      });
  });
  

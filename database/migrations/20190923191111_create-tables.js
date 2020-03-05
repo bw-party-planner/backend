85f49b76bfbd4dfd7e8388719028e9a95ce0810c
Suparada Saitalae <ann.suparada@gmail.com>
2020-03-04 20:18:56 -0600

    change  to "" on migration

--- database/migrations/20190923191111_create-tables.js
+++ database/migrations/20190923191111_create-tables.js
@@ -1,87 +1,87 @@
 
 exports.up = function(knex) {
   return knex.schema
-  .createTable('users', users => {
+  .createTable("users", users => {
     users.increments();
     users
-      .string('username', 255)
+      .string("username", 255)
       .notNullable()
       .unique();
-    users.string('password', 255).notNullable();
+    users.string("password", 255).notNullable();
   })
-  .createTable('parties', tbl => {
+  .createTable("parties", tbl => {
     tbl.increments();
-    tbl.string('party_name').notNullable();
-    tbl.string('guests').notNullable();
-    tbl.string('theme')
-    tbl.string('date')
-    tbl.integer('budget')
+    tbl.string("party_name").notNullable();
+    tbl.string("guests").notNullable();
+    tbl.string("theme")
+    tbl.string("date")
+    tbl.integer("budget")
     tbl
-      .integer('shopping_lists_id')
+      .integer("shopping_lists_id")
       .unsigned()
-      .references('id')
-      .inTable('shopping_lists')
-      .onDelete('CASCADE')
-      .onUpdate('CASCADE')
+      .references("id")
+      .inTable("shopping_lists")
+      .onDelete("CASCADE")
+      .onUpdate("CASCADE")
     tbl
-      .integer('todo_lists_id')
+      .integer("todo_lists_id")
       .unsigned()
-      .references('id')
-      .inTable('todo_lists')
-      .onDelete('CASCADE')
-      .onUpdate('CASCADE')
+      .references("id")
+      .inTable("todo_lists")
+      .onDelete("CASCADE")
+      .onUpdate("CASCADE")
       tbl
-      .integer('user_id')
+      .integer("user_id")
       .unsigned()
-      .references('id')
-      .inTable('users')
-      .onDelete('CASCADE')
-      .onUpdate('CASCADE')
+      .references("id")
+      .inTable("users")
+      .onDelete("CASCADE")
+      .onUpdate("CASCADE")
       .notNullable();
   })
   
 
-  .createTable('shopping_lists', tbl => {
+  .createTable("shopping_lists", tbl => {
     tbl.increments();
   })
-  .createTable('shopping_list_items', tbl => {
+  .createTable("shopping_list_items", tbl => {
     tbl.increments();
-    tbl.string('item').notNullable();
-    tbl.integer('price').notNullable();
-    tbl.boolean('purchased').defaultTo(false)
+    tbl.string("item").notNullable();
+    tbl.integer("price").notNullable();
+    tbl.boolean("purchased").defaultTo(false)
     tbl
-      .integer('shopping_list_id')
+      .integer("shopping_list_id")
       .unsigned()
-      .references('id')
-      .inTable('shopping_lists')
-      .onDelete('CASCADE')
-      .onUpdate('CASCADE')
+      .references("id")
+      .inTable("shopping_lists")
+      .onDelete("CASCADE")
+      .onUpdate("CASCADE")
   })
 
 
-  .createTable('todo_lists', tbl => {
+  .createTable("todo_lists", tbl => {
     tbl.increments();
   })
-  .createTable('todo_task', tbl => {
+  .createTable("todo_task", tbl => {
     tbl.increments();
-    tbl.string('task').notNullable()
-    tbl.boolean('completed').defaultTo(false)
+    tbl.string("task").notNullable()
+    tbl.boolean("completed").defaultTo(false)
     tbl
-      .integer('todo_lists_id')
+      .integer("todo_lists_id")
       .unsigned()
-      .references('id')
-      .inTable('todo_lists')
-      .onDelete('CASCADE')
-      .onUpdate('CASCADE');
+      .references("id")
+      .inTable("todo_lists")
+      .onDelete("CASCADE")
+      .onUpdate("CASCADE");
   })
 };
 
 exports.down = function(knex) {
     return knex.schema
-    .dropTableIfExists('todo_task')
-    .dropTableIfExists('todo_lists')
-    .dropTableIfExists('shopping_list_items')
-    .dropTableIfExists('shopping_lists')
-    .dropTableIfExists('parties')
-    .dropTableIfExists('users');
+    .dropTableIfExists("todo_task")
+    .dropTableIfExists("todo_lists")
+    .dropTableIfExists("shopping_list_items")
+    .dropTableIfExists("shopping_lists")
+    .dropTableIfExists("parties")
+    .dropTableIfExists("users");
 };

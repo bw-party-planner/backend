51e40905c6b4060072c200c2f5a8b947ccf9f66f
Suparada Saitalae <ann.suparada@gmail.com>
2020-03-04 18:54:13 -0600

    clean up

--- api/server.js
+++ api/server.js
@@ -7,10 +7,6 @@ const authRouter = require('../auth/auth-router.js');
 const authenticate = require('../auth/auth-model.js');
 const users = require('../users/users-router.js');
 const partiesRouter = require('../parties/parties-router.js');
-const picturesRouter = require('../pictures/pictures-router.js');
-const shoppingListsRouter = require('../shoppingList/shoppingLists-router.js');
-const todoListsRouter = require('../todoList/todoLists-router.js');
-const categoriesRouter = require('../categories/categories-router.js');
 
 
 server.use(express.json());
@@ -19,15 +15,8 @@ server.use(cors());
 server.use('/api/auth', authRouter);
 server.use('/api/parties', authenticate, partiesRouter);
 
-
 // for backend
 server.use('/api/users', users);
-
-// stretch goal
-server.use('/api/pictures', authenticate, picturesRouter);
-server.use('/api/shoppingLists', authenticate, shoppingListsRouter);
-server.use('/api/todoLists', authenticate, todoListsRouter);
-
 
 server.get('/', (req, res) => {
     res.send(`<h2>Check out API documentation at https://github.com/bw-party-planner/backend</h2>`)
